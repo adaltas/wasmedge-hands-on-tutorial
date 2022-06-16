eac4e46372399e934f3daf341b12ac9a56e42d38
gboutry <guillaume@adaltas.com>
2022-06-16 10:12:07 +0200

    fix(hosts): simplify hosts script

--- hosts.sh
+++ hosts.sh
@@ -1,11 +1,7 @@
-
 export $(vagrant ssh-config master | awk '{if($1) printf toupper($1) "=" $2 " ";}' | xargs)
 
+echo "Inventory:"
 
-env $ssh_env cat <<EOF > hosts
+cat <<EOF | tee hosts
 master ansible_host=$HOSTNAME ansible_port=$PORT ansible_user=$USER ansible_private_key_file=$IDENTITYFILE
 EOF
-
-
-echo "Inventory:"
-cat hosts
